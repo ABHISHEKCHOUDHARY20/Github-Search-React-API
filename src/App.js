8c4a7937f8f1e75caf43bb346a72f27417eebc15
abhishek <1704241@kiit.ac.in>
2020-09-09 20:02:25 +0530

    Created a search with form tag so that on click enter we get results without using a button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-
-function App() { 
+function App() {
   return (
     <div >
+      <form onSubmit={evt=>{
+        evt.preventDefault();
+        setInputValue(evt.target.elements.query.value);
+        }}>
+      <input className="github_search" type="text" name="query" placeholder="Search Github"/>
+      </form>
+     
      </div>
   );
 }
 
-
+export default App;
