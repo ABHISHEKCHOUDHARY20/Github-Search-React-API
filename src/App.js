add1f54d06c5fbb968841647c99ca35f883d6ee7
abhishek <1704241@kiit.ac.in>
2020-09-09 20:12:08 +0530

    Finally listed all the search result and displayed on the browser. Also made changes in the in the App.css

--- src/App.js
+++ src/App.js
@@ -23,15 +23,25 @@ function App() {
         
         });
   }, [inputValue]);
+
+  console.log(repos);
   return (
     <div >
       <form onSubmit={evt=>{
         evt.preventDefault();
         //debugger;
+        // console.log(evt.target.elements.query.value);
         setInputValue(evt.target.elements.query.value);
         }}>
       <input className="github_search" type="text" name="query" placeholder="Search Github"/>
       </form>
+      {isLoading?<div>Loading...</div>:null}
+      <ul className="repolist">{repos.map(repo=>{
+          return <li key={repo.id} className="repoitem">
+            <a href={repo.html_url} target="_blank">{repo.name}</a>
+            <p>{repo.description}</p>
+            </li>
+       })}</ul>
      </div>
   );
 }
