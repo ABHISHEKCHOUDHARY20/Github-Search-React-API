6d5f01ba9e916b2ed04eef4882901c490ca683c9
abhishek <1704241@kiit.ac.in>
2020-09-09 20:05:23 +0530

    Created 3 new states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
+
+// React.createElement('div")
+// https://api.github.com/search/repositories?q=html
 function App() {
+  const [inputValue, setInputValue] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [repos, setRepos] = React.useState([]);
   return (
     <div >
       <form onSubmit={evt=>{
@@ -9,7 +15,6 @@ function App() {
         }}>
       <input className="github_search" type="text" name="query" placeholder="Search Github"/>
       </form>
-     
      </div>
   );
 }
