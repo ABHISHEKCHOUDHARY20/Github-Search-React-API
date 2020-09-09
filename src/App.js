aa2fffe97ead597857dfd3497cd733acaa2eb5bf
abhishek <1704241@kiit.ac.in>
2020-09-09 20:07:35 +0530

    Featched the url to search git repositories using fetch()

--- src/App.js
+++ src/App.js
@@ -7,10 +7,27 @@ function App() {
   const [inputValue, setInputValue] = React.useState("");
   const [isLoading, setIsLoading] = React.useState(false);
   const [repos, setRepos] = React.useState([]);
+
+  React.useEffect(()=>{
+    if(!inputValue){
+      return;
+    } 
+    setIsLoading(true);
+    // 
+   fetch('https://api.github.com/search/repositories?q='+inputValue)
+        .then(response => response.json())
+        .then(data =>{
+          console.log(data);
+          setIsLoading(false);
+          setRepos(data.items)
+        
+        });
+  }, [inputValue]);
   return (
     <div >
       <form onSubmit={evt=>{
         evt.preventDefault();
+        //debugger;
         setInputValue(evt.target.elements.query.value);
         }}>
       <input className="github_search" type="text" name="query" placeholder="Search Github"/>
